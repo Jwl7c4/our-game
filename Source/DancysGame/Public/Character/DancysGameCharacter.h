fc514c0f9dee1146c729c3b0dee55c70ca7d0b3d
Jake Lowrey <jwl7c4@gmail.com>
2024-01-28 22:31:47 -0600

    player controller handles input. wip: player state

--- Source/DancysGame/Public/Character/DancysGameCharacter.h
+++ Source/DancysGame/Public/Character/DancysGameCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
+
 #include "DancysGameCharacter.generated.h"
 
 class USpringArmComponent;
@@ -49,17 +50,6 @@ public:
 	
 
 protected:
-
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-			
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 	// To add mapping context
 	virtual void BeginPlay();
